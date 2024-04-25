807066d117b097109dcf886749bb2ebe2753887f
workkwaw <86957130+workkwaw@users.noreply.github.com>
2024-04-25 20:39:16 +0000

    reading 100 1MB files and averaging time

--- Operations.java
+++ Operations.java
@@ -1,7 +1,31 @@
-class Operations {
-  static String[] filenames = {"txt/in1.txt", "txt/in2.txt", "txt/in3.txt",
-                               "txt/in4.txt", "txt/in5.txt"};
-  public static void main(String[] args) { runContiguousReads(); }
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 
-  static void runContiguousReads() {}
+class Operations {
+  public static void main(String[] args) throws IOException {
+    runContiguousReads();
+  }
+
+  static void runContiguousReads() throws IOException {
+    long total = 0;
+    File file;
+    FileInputStream str;
+    for (int i = 1; i <= 100; i++) {
+      byte[] buffer = new byte[1024];
+
+      file = new File("txt/me/in" + i + ".txt");
+
+      long time = System.nanoTime();
+
+      str = new FileInputStream(file);
+      str.read(buffer);
+      total += (System.nanoTime() - time);
+
+      time = 0;
+      str.close();
+    }
+
+    System.out.println(total / 100);
+  }
 }

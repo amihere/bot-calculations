cecd2b4b6fbcabcbd1b14c6f5c1fb8ad42289d1b
workkwaw <86957130+workkwaw@users.noreply.github.com>
2024-04-25 21:32:24 +0000

    update to accomodate mb file

--- Operations.java
+++ Operations.java
@@ -3,21 +3,38 @@ import java.io.FileInputStream;
 import java.io.IOException;
 
 class Operations {
-  public static void main(String[] args) throws IOException {
-    runContiguousReads();
+  public static void main(String[] args) throws Exception {
+    // runContiguousReadsMemory();
+    runContiguousReadsDisk();
   }
 
-  static void runContiguousReads() throws IOException {
+  static void runContiguousReadsMemory() throws Exception {
+    long total = 0;
+    File file = new File("res/megabyte.txt");
+    FileInputStream str = new FileInputStream(file);
+    byte[] buffer = new byte[1048576];
+    str.read(buffer);
+    str.close();
+
+    byte b;
+    long time = System.nanoTime();
+    for (int i = 0; i < 1048576; i++) {
+      b = buffer[i];
+    }
+    total += (System.nanoTime() - time);
+
+    System.out.println(total);
+  }
+
+  static void runContiguousReadsDisk() throws IOException {
     long total = 0;
     File file;
     FileInputStream str;
     for (int i = 1; i <= 100; i++) {
-      byte[] buffer = new byte[1024];
-
-      file = new File("txt/me/in" + i + ".txt");
+      byte[] buffer = new byte[1048576];
 
       long time = System.nanoTime();
-
+      file = new File("res/me/in" + i + ".txt");
       str = new FileInputStream(file);
       str.read(buffer);
       total += (System.nanoTime() - time);
